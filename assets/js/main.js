dad047dbfc95d9895d4a739dee64ff61e65478ad
Johannes <joshadow@gmail.com>
2018-03-08 06:22:47 -0800

    finished first pass

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,285 @@
-
-$(function() {
+let SequenceMode = undefined;
+const SequenceModeStoreStr = "App.Settings.SequenceMode";
 
 const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
 
-// Dom Manipulation
-const gist_form     = query('#gist-form');
-const gist_field    = query('#gist-field');
-const reply_content = query('#reply-content');
-
-// The headers used to identify each row in the csv
-const TableHeaders = {
-  seed : "Seed",                 // DNA 6-Mer
-  via1 : "Cell viability HeyA8",
-  std1 : "STDEV (HeyA8)",
-  via2 : "Cell viability M565",
-  std2 : "STDEV (M565)",
-  avg  : "Average"                // calculated when needed from via1, via2
-};
+// for console debugging
+var table;
+var filteredRowCount;
+var filteredRowData;
 
 
-// Mer6 Data Constructor
-function Mer6(seed, via1, std1, via2, std2) {
-  this.seed = seed;
-  this.via1 = +via1;
-  this.std1 = +std1;
-  this.via2 = +via2;
-  this.std2 = +std2;
-}
+$(document).ready(function() {
 
-Mer6.prototype.getAvg = function() {
-  return (this.via1 + this.via2) / 2;
-}
+  // Dom Manipulation
+  let tableEl       = $('#data-table');
+  let settingsEl    = $('#settings-content');
+  let searchBoxEl   = $('#data-search-box');
+  let searchEl      = $('#data-search-field');
+  let modeBoxEl     = $('#data-mode-box');
+  let modeEl        = $('#data-mode-field');
+  let resetButtonEl = $('#reset-button');
+  let saveButtonEl  = $('#save-button');
 
-Mer6.prototype.toCSVRow = function(){
-  return [
-    this.seed,
-    this.via1,
-    this.std1,
-    this.via2,
-    this.std2,
-    this.getAvg(),
-  ].join(',') + "\n";
-}
+  // given a potential input seed, cleans it up
+  // - Can only be 6 chars long
+  // - All uppercase
+  // - U or T based on if in RNA or DNA mode
+  var SanitizeSequence = function(str, len)
+  {
+    if(len === undefined)
+      len = str.length;
+    var chars = str.substring(0,len).toUpperCase().split('');
+    for(var i = 0; i < chars.length; ++i)
+    {
+      if(!['A','C','T','G','U'].includes(chars[i]))
+      {
+        chars.splice(i,1);
+        --i;
+      }
+      else if(SequenceMode === "RNA" && chars[i] == 'T') 
+        chars[i] = 'U';
+      else if(SequenceMode === "DNA" && chars[i] == 'U') 
+        chars[i] = 'T';
+    }
+
+    return chars.join('');
+  }
+
+  // small helper function for only 
+  var RNAtoDNA = function (str) {
+    return str.split('U').join('T');
+  }
+  var DNAtoRNA = function (str) {
+    return str.split('T').join('U');
+  }
+
+  var GetModeFromHash = function() {
+    var i = location.hash.indexOf(':');
+
+    if(i == -1)
+    {
+      console.log("INVALID HASH STRING - NO MODE");
+      return undefined;
+    }
+
+    return location.hash.substring(1,i);
+  }
+
+  var GetSequenceFromHash = function() {
+    var i = location.hash.indexOf(':');
+    if(i == -1)
+      i = 0;
+
+    return location.hash.substring(i+1);
+    return raw;
+  }
+
+  // Builds the hash string 
+  var BuildHash = function(sequence)
+  {
+    let mode = "";
+    if(SequenceMode !== undefined)
+    {
+      if(SequenceMode === "RNA" && GetModeFromHash() != undefined)
+        mode = "RNA";
+      else if(SequenceMode === "DNA")
+        mode = "DNA";
+    }
+    return `${mode}${mode == "" ? "" : ":"}${sequence}`; 
+  }
 
 
-d3.csv("data/sample.csv")
-  .row( r => {
-    return new Mer6(
-      r[TableHeaders.seed],
-      r[TableHeaders.via1],
-      r[TableHeaders.std1],
-      r[TableHeaders.via2],
-      r[TableHeaders.std2],
-    );
-  })
-  .get( (error, dataArr) => {
-    if(error) throw error;
+  d3.csv("data/6Mer_Data.csv")
+    .row( r => { return new Mer6(r); })
+    .get( (error, dataArr) => {
+      if(error) throw error;
 
-    // Quick lookup map of seed to index
-    let dataMap = {};
+      // A lookup map of seed to index
+      let dataMap = {};
+      for(var i = 0; i < dataArr.length; ++i)
+        dataMap[dataArr[i].seed] = +i;
 
-    for(var i = 0; i < dataArr.length; ++i)
-      dataMap[dataArr[i].seed] = +i;
-
-    console.log(dataArr);
-    console.log(dataMap);
-
-    Mer6ArrayToCSV(dataArr);
-  });
+      InitTable(dataArr, dataMap)
+    });
 
 
-// takes an array with a 'columns' field
-var Mer6ArrayToCSV = function(dataArr)
-{
-  // Set up headers
-  let csv = dataArr.columns.join(",") + "," + TableHeaders.avg + "\n";
 
-  for(var i = 0; i < dataArr.length; ++i)
-    csv += dataArr[i].toCSVRow();
-  
-  return csv;
-}
+  var InitTable = function(dataArr, dataMap) {
+    table = tableEl.DataTable({
+      data: dataArr,
+      dom: `
+  <"row"
+    <"col t-len" <"float-left" l > <"float-right" p > <"float-left" i > >
+  >
+  r
+  t
+  <"row"
+    <"col t-len" <"float-left" l > <"float-right" p > <"float-left" i > >
+  >`,
+      paging: true,
+      search: {
+        smart: false
+      },
+      searchDelay: 100,
+      processing: true,
+      stateSave: true,
+      lengthMenu: [[ 10, 50, 100, 500, -1], [ 10, 50, 100, 500, "All (slow)"]],
+      columns: [
+        // Seed
+        { data: "seed", title: TableHeaders.seed, searchable: true,  orderable: true,  className: "h-seed", render: function ( data, type, row, meta ) { 
+            return `<span class="seq-dna">${RNAtoDNA(data)}</span><span class="hidden">.</span><span class="seq-rna">${DNAtoRNA(data)}</span>`
+          } 
+        },
+        // Vitality 1
+        { data: "via1", title: TableHeaders.via1, searchable: false, orderable: true,  className: "h-via"  },
+        // STDEV 1
+        { data: "std1", title: TableHeaders.std1, searchable: false, orderable: false, className: "h-std"  },
+        // Vitality 2
+        { data: "via2", title: TableHeaders.via2, searchable: false, orderable: true,  className: "h-via"  },
+        // STDEv 2
+        { data: "std2", title: TableHeaders.std2, searchable: false, orderable: false, className: "h-std"  },
+        // Average
+        { data: "avg",  title: TableHeaders.avg,  searchable: false, orderable: true,  className: "h-avg"  },
+      ]
+    });
+
+    // Helper function to sanitize search field and update table
+    var PerformSearch = function(updateHash) {
+      let content = SanitizeSequence(searchEl.val(), 6);
+      searchEl.val(content);
+
+      if(updateHash === undefined || updateHash === true)
+        location.hash = BuildHash(content);
+
+      console.log(`searching for: ${DNAtoRNA(content)}`);
+      // console.log(location.hash.substring(1));
+      regExSearch = ".*" + DNAtoRNA(content) + ".*";
+      table.column(0).search(regExSearch, true, false).draw();
+    }
+
+    // true  = RNA
+    // false = DNA
+    var SetMode = function(val, updateSearch) {
+      // default for invalid inputs is RNA
+      val = (!val ? "RNA" : val);
+      if(SequenceMode !== undefined && SequenceMode == val)
+        return;
+      SequenceMode = val;
+
+      console.log(`*** MODE IS NOW: ${SequenceMode}`);
+      // Update table view
+      if(SequenceMode !== "RNA") {
+        tableEl.addClass("mode-dna");
+        tableEl.removeClass("mode-rna");
+      } else {
+        tableEl.addClass("mode-rna");
+        tableEl.removeClass("mode-dna");
+      }
+
+      // Update toggle input
+      modeEl.bootstrapToggle(SequenceMode === "RNA" ? "on" : "off");
+   
+      // Store mode
+      window.localStorage.setItem(SequenceModeStoreStr, SequenceMode);
+    }
+
+
+    // If mode given in hash use that
+    let initialMode = GetModeFromHash();
+    // If no mode in hash, try to use saved
+    if(initialMode === undefined)
+    {
+      initialMode = window.localStorage.getItem(SequenceModeStoreStr);
+    }
+
+    SetMode(initialMode);
+
+    // Store filtered rows for saving
+    table.on('search.dt', function() {
+      filteredRowCount = table.rows( { filter : 'applied'} ).nodes().length;
+      filteredRowData  = table.rows( { filter : 'applied'} ).data();    
+    });
+    
+    // Check hash for data 
+    let initialSearch = GetSequenceFromHash();
+    // If no sequence in hash, attempt to load from past search state
+    if(initialSearch === "")
+      initialSearch = table.state().search.search;
+    // Update Search field
+    searchEl.val(initialSearch);
+    PerformSearch();
+
+    searchEl.on('input', (e) => {
+      // console.log("------ On Search Input");
+      PerformSearch();
+    });
+
+    modeEl.on('change', (e) => {
+      // console.log("------ On Mode Switch Change");
+      let newMode = modeEl.prop('checked') ? "RNA" : "DNA";
+      SetMode(newMode);
+      PerformSearch();
+    });
+
+    resetButtonEl.on('click' , (e) => {
+      window.localStorage.clear();
+      window.location = window.location.href.replace(window.location.hash, '');
+    });
+
+    saveButtonEl.on('click', (e) => {
+      let csvData = dataArr.columns.join(",") + "," + TableHeaders.avg + "\n";
+      for(var i = 0; i < filteredRowCount; ++i)
+        csvData += filteredRowData[i].toCSVRow();
+
+      var filterStr = searchEl.val();
+      if(filterStr == "")
+        filterStr = "all";
+      var csvFileName = `6mer_${filterStr}_${SequenceMode}.csv`
+
+      var blob = new Blob([csvData]);
+      if (window.navigator.msSaveOrOpenBlob)  // IE hack; see http://msdn.microsoft.com/en-us/library/ie/hh779016.aspx
+          window.navigator.msSaveBlob(csvData, csvFileName);
+      else
+      {
+          var a = window.document.createElement("a");
+          a.href = window.URL.createObjectURL(blob, {type: "text/plain"});
+          a.download = csvFileName;
+          document.body.appendChild(a);
+          a.click();  // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
+          document.body.removeChild(a);
+      }
+      
+    });
+
+    // Handle user manually changing hash string
+    if("onhashchange" in window) {
+      window.addEventListener("hashchange", () => {
+        console.log("------ On Hash Change");
+        console.log("PRE HASH MODE");
+        SetMode(GetModeFromHash());
+        console.log("POST HASH MODE");
+
+        let newVal = GetSequenceFromHash();
+        console.log(`NEW SEQUENCE: ${newVal}`);
+        if(newVal === searchEl.val())
+        {
+          console.log("SEARCH IS SAME!");
+          return;
+        }
+
+        searchEl.val(newVal);
+        PerformSearch();
+      });
+
+      // Everything is set up, show settings
+      searchEl.prop("disabled", false);
+      resetButtonEl.prop("disabled", false);
+      saveButtonEl.prop("disabled", false);
+    }
+
+  }
+
 
 });
---
+++ assets/js/mer6data.js
@@ -0,0 +1,71 @@
+// The headers used to identify each row in the csv
+const TableHeaders = {
+  seed : "Seed",                 // DNA 6-Mer
+  via1 : "Cell viability HeyA8",
+  std1 : "STDEV (HeyA8)",
+  via2 : "Cell viability M565",
+  std2 : "STDEV (M565)",
+  avg  : "Average"               // calculated when needed from via1, via2
+};
+
+// Mer6 Data Default Constructor
+function Mer6(obj, via1, std1, via2, std2, avg) {
+  // obj is seed string
+  if(typeof(obj) === "string")
+    this.set(obj, via1, std1, via2, std2, avg);
+  // obj is whole row object
+  else if(Array.isArray("array"))
+    this.set( obj[0], obj[1], obj[2], obj[3], obj[4], obj[5]);
+  else
+    this.set(
+      obj[TableHeaders.seed],
+      obj[TableHeaders.via1],
+      obj[TableHeaders.std1],
+      obj[TableHeaders.via2],
+      obj[TableHeaders.std2],
+      obj[TableHeaders.avg],
+    );
+}
+
+Mer6.prototype.set = function(seed, via1, std1, via2, std2, avg) {
+  this.seed = seed;
+  this.via1 = +via1;
+  this.std1 = +std1;
+  this.via2 = +via2;
+  this.std2 = +std2;
+  if(avg !== undefined)
+    this.avg = +avg;
+  else
+    this.updateAvg();
+}
+
+Mer6.prototype.updateAvg = function() {
+  this.avg = (this.via1 + this.via2) / 2;
+}
+
+Mer6.prototype.toCSVRow = function() {
+  return this.toArray().join(',') + "\n";
+}
+
+Mer6.prototype.toArray = function() {
+  return [
+    this.seed,
+    this.via1,
+    this.std1,
+    this.via2,
+    this.std2,
+    this.avg
+  ];
+}
+
+// takes an array with a 'columns' field
+var Mer6ArrayToCSV = function(dataArr)
+{
+  // Set up headers
+  let csv = dataArr.columns.join(",") + "," + TableHeaders.avg + "\n";
+
+  for(var i = 0; i < dataArr.length; ++i)
+    csv += dataArr[i].toCSVRow();
+  
+  return csv;
+}
