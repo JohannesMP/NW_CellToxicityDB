0c1bab6e6d36087c6e5793c39857dd8fccf0b29e
Johannes <joshadow@gmail.com>
2020-05-31 15:55:39 -0700

    Revert viability color gradient

--- assets/js/render.js
+++ assets/js/render.js
@@ -11,20 +11,21 @@ var renderNumColorRange = function( data, type, row, meta ) {
     var rgb_orange = [255,255,128]
     var rgb_green = [0,255,0];
 
-    var a_low  = 0.5;
+    var a_low  = 0;
     var a_high = 0.5;
 
+    var midpoint = 60;
     var t = 0;
 
-    if(val < 60) {
-        low  = rgb_orange.concat(a_low);
+    if(val < midpoint) {
+        low  = rgb_red.concat(a_low);
         high = rgb_red.concat(a_high);
-        t = 1 - (val / 60);
+        t = 1 - (val / midpoint);
     }
-    else if(val > 60) {
-        low  = rgb_orange.concat(a_low);
+    else if(val > midpoint) {
+        low  = rgb_green.concat(a_low);
         high = rgb_green.concat(a_high);
-        t = (val / 60) - 1;
+        t = (val / midpoint) - 1;
     }
 
     var rgba = lerpArray(low, high, t);
